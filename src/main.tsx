4d500b2ea3264db7258b8827da2c3e7dc66066ef
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-07-06 23:17:50 +0300

    refactor: apply prettier

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,6 @@ import ErrorBoundary from './components/ErrorBoundary';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      {' '}
-      {/* Wrap App with ErrorBoundary */}
       <App />
     </ErrorBoundary>
   </React.StrictMode>,
