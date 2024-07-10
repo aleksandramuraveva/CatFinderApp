1d0e719621200e07247e30e825e539037e861fa0
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-07-10 03:43:25 +0300

    refactor: apply prettier

--- src/components/ErrorButton/index.tsx
+++ src/components/ErrorButton/index.tsx
@@ -1,33 +1,20 @@
 import React from 'react';
 import './styles.css';
 
-
 interface ErrorButtonProps {
   onError: () => void;
 }
 
-
-const ErrorButton: React.FC<ErrorButtonProps> = ({onError}): JSX.Element => {
-
+const ErrorButton: React.FC<ErrorButtonProps> = ({ onError }): JSX.Element => {
   const handleClick = () => {
     onError();
   };
 
   return (
-
-
     <button className="error-button" onClick={handleClick}>
-         Crash the app
+      Crash the app
     </button>
-
-
-    );
-
-
-
-
+  );
 };
-
-
 
 export default ErrorButton;
