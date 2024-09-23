1c724db9a6d146231184a7c7d5f3b523645c6cdf
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-09-23 23:56:09 +0300

    fix: correct mock data in DetailedCard test

--- src/components/DetailedCard/DetailedCard.test.tsx
+++ src/components/DetailedCard/DetailedCard.test.tsx
@@ -4,9 +4,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import DetailedCard from '.';
 import { ThemeProvider } from '../../contexts/themeContext.tsx';
+import { Actress } from '../../types';
 
 const mockActress: Actress = {
-  id: '1',
+  id: 1,
   name: 'Actress 1',
   image: 'image1.jpg',
   birth_year: 1980,
@@ -16,17 +17,25 @@ const mockActress: Actress = {
   biography: 'Biography 1',
 };
 
+// global.fetch = vi.fn(() =>
+//   Promise.resolve({
+//     json: () => Promise.resolve(mockActress),
+//   }),
+// );
+
 global.fetch = vi.fn(() =>
   Promise.resolve({
+    ok: true,
+    status: 200,
     json: () => Promise.resolve(mockActress),
-  }),
+  } as Response),
 );
 
 test('displays a loading indicator while fetching data', async () => {
   render(
     <BrowserRouter>
       <ThemeProvider>
-        <DetailedCard />
+        <DetailedCard actress={mockActress} setSelectedActress={() => {}} />
       </ThemeProvider>
     </BrowserRouter>,
   );
